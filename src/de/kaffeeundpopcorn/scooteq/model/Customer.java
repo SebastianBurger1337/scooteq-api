97ea37076765652b40de472f7677474c6ab803ea
Sebastian Burger <sebastian.burger@otto.de>
2019-09-08 18:06:29 +0200

    Changes

--- src/de/kaffeeundpopcorn/scooteq/model/Customer.java
+++ src/de/kaffeeundpopcorn/scooteq/model/Customer.java
@@ -1,5 +1,37 @@
 package de.kaffeeundpopcorn.scooteq.model;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="customer")
+@EntityListeners(AuditingEntityListener.class)
 public class Customer
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private int customerStatusId;
+
+    private boolean driversLicense;
+
+    private String address;
+
+    private String city;
+
+    private String postalCode;
+
+    private String country;
+
+    private Date birthDate;
+
+
+
 }
