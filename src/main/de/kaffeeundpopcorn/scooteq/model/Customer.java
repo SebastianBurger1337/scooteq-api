4573511942f9409a56ee61f92991781941e14072
SebastianBurger1337 <sebastian.burger@otto.de>
2019-09-16 10:10:11 +0200

    Added customer Spring Entity

--- src/main/de/kaffeeundpopcorn/scooteq/model/Customer.java
+++ src/main/de/kaffeeundpopcorn/scooteq/model/Customer.java
@@ -6,32 +6,142 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name="customer")
+@Table(name = "customer")
 @EntityListeners(AuditingEntityListener.class)
 public class Customer
 {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "CustomerId", nullable = false)
+    private Long CustomerId;
 
+    @Column(name = "FirstName")
     private String firstName;
 
+    @Column(name = "LastName")
     private String lastName;
 
+    @Column(name = "CustomerStatusId", nullable = false)
     private int customerStatusId;
 
+    @Column(name = "DriversLicense")
     private boolean driversLicense;
 
+    @Column(name = "Address")
     private String address;
 
+    @Column(name = "City")
     private String city;
 
+    @Column(name = "PostalCode")
     private String postalCode;
 
+    @Column(name = "Country")
     private String country;
 
+    @Column(name = "BirthDate")
     private Date birthDate;
 
+    //region Getters and setters
+    public Long getCustomerId()
+    {
+        return CustomerId;
+    }
 
+    public void setCustomerId(Long customerId)
+    {
+        this.CustomerId = customerId;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public int getCustomerStatusId()
+    {
+        return customerStatusId;
+    }
+
+    public void setCustomerStatusId(int customerStatusId)
+    {
+        this.customerStatusId = customerStatusId;
+    }
+
+    public boolean isDriversLicense()
+    {
+        return driversLicense;
+    }
+
+    public void setDriversLicense(boolean driversLicense)
+    {
+        this.driversLicense = driversLicense;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getPostalCode()
+    {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode)
+    {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry()
+    {
+        return country;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public Date getBirthDate()
+    {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate)
+    {
+        this.birthDate = birthDate;
+    }
+    //endregion
 
 }
