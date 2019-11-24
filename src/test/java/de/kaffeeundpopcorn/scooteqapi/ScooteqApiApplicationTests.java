e0a9d17a3d94a0bee7c0d105533f5c67a1ac2500
SebastianBurger1337 <sebastian.burger@otto.de>
2019-11-24 18:04:41 +0100

    Moved test folder to src

--- src/test/java/de/kaffeeundpopcorn/scooteqapi/ScooteqApiApplicationTests.java
+++ src/test/java/de/kaffeeundpopcorn/scooteqapi/ScooteqApiApplicationTests.java
@@ -1,13 +1,18 @@
 package de.kaffeeundpopcorn.scooteqapi;
 
+import de.kaffeeundpopcorn.scooteq.controller.ApiController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ScooteqApiApplicationTests {
+
+	@Autowired
+	public ApiController apiController;
 
 	@Test
 	public void contextLoads() {
